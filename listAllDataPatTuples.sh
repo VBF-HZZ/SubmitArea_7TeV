f1e4a6edcb1987fd4120c1684e3683780f0a4067
Hengne Li <Hengne.Li@cern.ch>
2014-07-14 06:27:24 -0400

    add listAllDataPatTuples.sh to do listPatTuples for all data sets

---
+++ listAllDataPatTuples.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+./listPatTuples.sh data/44X/Legacy_06.13/JanA_Aug
+mv patTuples Data_JanA_Aug
+./listPatTuples.sh data/44X/Legacy_06.13/JanA_Aug_MuEG
+mv patTuples Data_JanA_Aug_MuEG
+./listPatTuples.sh data/44X/Legacy_06.13/JanA_Jul1
+mv patTuples Data_JanA_Jul1
+./listPatTuples.sh data/44X/Legacy_06.13/JanA_Jul1_MuEG
+mv patTuples Data_JanA_Jul1_MuEG
+./listPatTuples.sh data/44X/Legacy_06.13/JanA_Jul2
+mv patTuples Data_JanA_Jul2
+./listPatTuples.sh data/44X/Legacy_06.13/JanA_Jul2_MuEG
+mv patTuples Data_JanA_Jul2_MuEG
+./listPatTuples.sh data/44X/Legacy_06.13/JanA_Oct
+mv patTuples Data_JanA_Oct
+./listPatTuples.sh data/44X/Legacy_06.13/JanA_Oct_MuEG
+mv patTuples Data_JanA_Oct_MuEG
+./listPatTuples.sh data/44X/Legacy_06.13/JanB
+mv patTuples Data_JanB
+./listPatTuples.sh data/44X/Legacy_06.13/JanB_MuEG
+mv patTuples Data_JanB_MuEG
