573ed6147529aabead498850bee239eb9294834f
Hengne Li <Hengne.Li@cern.ch>
2014-07-14 06:28:59 -0400

    write out lists of data sets in submitAllData.sh

--- submitAllData.sh
+++ submitAllData.sh
@@ -1,8 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-for f in Submission_Data_*
-  do
+list=""
+list="${list} Submission_Data_JanA_Aug "
+list="${list} Submission_Data_JanA_Aug_MuEG "
+list="${list} Submission_Data_JanA_Jul1 "
+list="${list} Submission_Data_JanA_Jul1_MuEG "
+list="${list} Submission_Data_JanA_Jul2 "
+list="${list} Submission_Data_JanA_Jul2_MuEG "
+list="${list} Submission_Data_JanA_Oct "
+list="${list} Submission_Data_JanA_Oct_MuEG "
+list="${list} Submission_Data_JanB "
+list="${list} Submission_Data_JanB_MuEG "
 
+for f in $list ;
+do
   ./Submit.sh $f
-
 done
