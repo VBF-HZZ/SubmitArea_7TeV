63cac7c4d5f1d472e30d1fdb899cc1a7d046a7c4
Hengne Li <Hengne.Li@cern.ch>
2014-07-18 04:53:37 -0400

    print out the config files of failed jobs

--- checkForCorruptFiles.sh
+++ checkForCorruptFiles.sh
@@ -5,9 +5,12 @@ echo "Following jobs have problem: "
 for file in errFiles/*.err
 do 
   error=`grep FatalRootError $file`
+  error="${error}`grep \"job killed\" $file`"
+
   if [ "$error" != "" ]; then
-    echo $file
-   # echo "  Error: $error" 
+    #echo $file
+    #echo "  Error: $error" 
+    ls Submission*/`basename $file .err`.py
   fi
 done
 
